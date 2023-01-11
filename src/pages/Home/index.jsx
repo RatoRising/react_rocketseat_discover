89dbb8c0d39eec49a4bd70c275c4e6cccf8b1058
RatoRising <reinaneves@hotmail.com>
2023-01-11 18:43:02 -0300

    add student handling function and imutability concept understanding

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,18 +3,31 @@ import './styles.css';
 import { Card } from '../../components/Card';
 
 export function Home() {
-	const [ studentName, setStudentName ] = useState();
+	const [ studentName, setStudentName ] = useState('');
+	const [ students, setStudents ] = useState([]);
+
+	function handleAddStudents() {
+		const newStudent = {
+			name: studentName,
+			time: new Date().toLocaleTimeString('pt-br', {
+				hour: '2-digit',
+				minute: '2-digit',
+				second: '2-digit'
+			})
+		};
+		setStudents((prevState) => [ ...prevState, newStudent ]);
+	}
 
 	return (
 		<div className="container">
 			<h1>Presence List</h1>
-			<h1>Name: {studentName}</h1>
-			<input type="text" placeholder="Type your name..." onChange={(e) => setStudentName(e.target.value)} />
-			<button type="button">Add</button>
 
-			<Card name="Rato" time="10:20" />
-			<Card name="Marrato" time="10:21" />
-			<Card name="Risging" time="10:22" />
+			<input type="text" placeholder="Type your name..." onChange={(e) => setStudentName(e.target.value)} />
+			<button type="button" onClick={handleAddStudents}>
+				Add Student
+			</button>
+
+			{students.map((student) => <Card name={student.name} time={student.time} />)}
 		</div>
 	);
 }
