be5b8842b7ea5a6febc14d4f2d63de4a64938f94
RatoRising <reinaneves@hotmail.com>
2023-01-11 20:22:01 -0300

    add react useEffect and async api request

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import './styles.css';
 import { Card } from '../../components/Card';
 
 export function Home() {
 	const [ studentName, setStudentName ] = useState('');
 	const [ students, setStudents ] = useState([]);
+	const [ user, setUser ] = useState({
+		name: '',
+		avatar: ''
+	});
 
 	function handleAddStudents() {
 		const newStudent = {
@@ -18,16 +22,47 @@ export function Home() {
 		setStudents((prevState) => [ ...prevState, newStudent ]);
 	}
 
+	// useEffect using async function recommended
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch('https://api.github.com/users/ratorising');
+			const data = await response.json();
+			setUser({
+				name: data.name,
+				avatar: data.avatar_url
+			});
+		}
+		fetchData();
+	}, []);
+
+	/* // useEffect sinc
+	useEffect(() => {
+		fetch('https://api.github.com/users/ratorising')
+		.then((response) => response.json())
+		.then((data) => {
+			setUser({
+				name: data.name,
+				avatar: data.avatar_url
+			});
+		});
+	}, []); */
+
 	return (
 		<div className="container">
-			<h1>Presence List</h1>
+			<header>
+				<h1>Presence List</h1>
+				<div>
+					<strong>{user.name}</strong>
+					<img src={user.avatar} alt="github profile picture" />
+				</div>
+			</header>
 
 			<input type="text" placeholder="Type your name..." onChange={(e) => setStudentName(e.target.value)} />
 			<button type="button" onClick={handleAddStudents}>
 				Add Student
 			</button>
 
-			{students.map((student) => <Card name={student.name} time={student.time} />)}
+			{students.map((student) => <Card key={student.time} name={student.name} time={student.time} />)}
 		</div>
 	);
 }
